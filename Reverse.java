ff6cc49dd66cfe3e75d073c343b3895688beef11
Sergey <Sergeev_sergey99@mail.ru>
2019-01-29 18:55:16 +0300

    Finish Reverse

--- Reverse.java
+++ Reverse.java
@@ -1,18 +1,26 @@
-import java.util.*;
-import java.io.*;
-  
-public class Reverse
-{
-  public static void main(String[] args)
-  {
+import java.util.Scanner;
 
-	Scanner reader = new Scanner(System.in);  // Reading from System.in
-	System.out.println("Enter a number: ");
-	int n = reader.nextInt(); // Scans the next token of the input as an int.
-	//once finished
-	reader.close();
-    
-    System.out.print("  *  "+n);
-    
-  }
+public class Reverse {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        String input = reader.nextLine();
+        reader.close();
+
+        char[] reverseInput = input.toCharArray();
+        int begin = 0;
+        int end = reverseInput.length - 1;
+        char temp;
+        while (end > begin) {
+            temp = reverseInput[begin];
+            reverseInput[begin] = reverseInput[end];
+            reverseInput[end] = temp;
+            end--;
+            begin++;
+        }
+
+
+        System.out.print(reverseInput);
+
+    }
 }
--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum
 {
   public static void main(String[] args)
   {
-    int i, sum = 0;
+    int sum = 0;
     for(String item:args)
       sum+=Integer.parseInt(item);
     System.out.print(sum);
