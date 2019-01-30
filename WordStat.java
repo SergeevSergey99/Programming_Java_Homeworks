0838e4ca702ef02ad64e5ed45ca7befc7be6604a
Sergey <Sergeev_sergey99@mail.ru>
2019-01-30 19:21:24 +0300

    update WordStat

--- WordStat.java
+++ WordStat.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordStat {
     public static void main(String[] args) {
+
+        try {
+            Scanner reader = new Scanner(new File("inputWordStat.txt"));
+            while (reader.hasNext()) {
+                System.out.println(reader.next().toLowerCase().replaceAll("\\p{Punct}", ""));
+//-------------------------------------------------------
+
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
