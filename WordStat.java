4ba6622c6ec8b7c813ad74c460c75e840a08c503
Sergey <Sergeev_sergey99@mail.ru>
2019-01-31 20:36:41 +0300

    finish WordStat without output in file

--- WordStat.java
+++ WordStat.java
@@ -5,16 +5,32 @@ import java.util.Scanner;
 public class WordStat {
     public static void main(String[] args) {
 
-        StringBuilder text = new StringBuilder();
+        String text;
 
         try {
             Scanner reader = new Scanner(new File("inputWordStat.txt"));
+            StringBuilder textBuilder = new StringBuilder();
             while (reader.hasNext()) {
-                text.append(reader.next().toLowerCase().replaceAll("\\p{Punct}", "")).append(" ");
+                textBuilder.append(reader.next().toLowerCase().replaceAll("\\p{Punct}", "")).append(" ");
 //-------------------------------------------------------
-                }
+            }
+            text = textBuilder.toString();
             reader.close();
             System.out.println(text);
+            String[] textArray = text.split(" ");
+            Integer[] textCount = new Integer[textArray.length];
+            for (int i = 0; i < textArray.length; i++) {
+                if (textArray[i].equals(" ")) continue;
+                textCount[i] = 1;
+                System.out.print(textArray[i] + " ");
+                for (int j = i + 1; j < textArray.length; j++) {
+                    if (textArray[j].equals(textArray[i])) {
+                        textCount[i]++;
+                        textArray[j] = " ";
+                    }
+                }
+                System.out.println(textCount[i]);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
