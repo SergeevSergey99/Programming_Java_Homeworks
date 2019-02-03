549e3d5d85f1d300ec558da4f5b0a80fcbd5d3fe
Sergey <Sergeev_sergey99@mail.ru>
2019-02-03 20:20:24 +0300

    finish MyScanner

--- MyScanner.java
+++ MyScanner.java
@@ -30,23 +30,4 @@ class MyScanner {
         }
         return lineBuilder.toString();
     }
-/*
-    private long nextLong() {
-        while ('0' > c || c > '9') {
-            nextChar();
-        }
-        long value = c - '0';
-        nextChar();
-        while ('0' <= c && c <= '9') {
-            value *= 10;
-            value += c - '0';
-            nextChar();
-        }
-        return value;
-    }
-
-    public int nextInt() {
-        long longValue = nextLong();
-        return (int) longValue;
-    }*/
 }
