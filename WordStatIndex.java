5ba3df7a90bd288729d580b1c59d0b2f0cf5f0b8
Sergey <Sergeev_sergey99@mail.ru>
2019-02-19 22:32:54 +0300

    WordStatIndex Update add output to file

--- WordStatIndex.java
+++ WordStatIndex.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,44 +13,34 @@ public class WordStatIndex {
         try {
             Scanner reader = new Scanner(new File("inputWordStat.txt"));
             int i = 0;
+
             while (reader.hasNext()) {
                 i++;
                 String text;
                 text = reader.next().toLowerCase().replaceAll("\\p{Punct}", "");
-                int index = arrayList.indexOf(text);
-                if (arrayList.contains(text)) {
+                int index = arrayList.indexOf("\n" + text);
+                if (arrayList.contains("\n" + text)) {
                     arrayList.set(1 + index, String.valueOf(Integer.parseInt(arrayList.get(index + 1)) + 1));
                     arrayList.set(index + 2, arrayList.get(index + 2) + " " + i);
                 } else {
-                    arrayList.add(text);
+                    arrayList.add("\n" + text);
                     arrayList.add(String.valueOf(1));
                     arrayList.add(String.valueOf(i));
                 }
 
             }
 
+            PrintWriter out = new PrintWriter("outputWordStatIndex.txt");//args[1]);
+
+            for (String item:arrayList) {
+                out.print(item + " ");
+            }
+
             reader.close();
+            out.close();
 
             System.out.println(arrayList);
-            /*
-            String[] textArray = text.split(" ");
-            Integer[] textCount = new Integer[textArray.length];
 
-            PrintWriter out = new PrintWriter(args[1]);
-
-            for (int i = 0; i < textArray.length; i++) {
-                if (textArray[i].equals(" ")) continue;
-                textCount[i] = 1;
-                out.print(textArray[i] + " ");
-                for (int j = i + 1; j < textArray.length; j++) {
-                    if (textArray[j].equals(textArray[i])) {
-                        textCount[i]++;
-                        textArray[j] = " ";
-                    }
-                }
-                out.println(textCount[i]);
-            }
-            out.close();*/
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
